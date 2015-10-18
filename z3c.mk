49c2e57f7eebe6987863f116c82d07e5914dc69a
AdrianDC <radian.dc@gmail.com>
2015-10-18 19:32:09 +0200

    z3c: Implement Doze Sensors detection
     * Add gestures panel to the DeviceSettings
    
     * Features :
       - Hand wave Doze if device is not face down
       - Pick-up Doze if was face down or taken in hand
       - Pocket Doze if device unstowed vertically
       - Proximity Wake if device is not face down
    
     * Sensors :
       - Proximity sensor used for all the features
       - Accelerometer and Magnetometer used for the orientation
       - Accelerometer put in idle for the Pick-up with face up
         - This sensor will go in deep-sleep with the phone
    
      * Changes depends on :
           huashan: Implement Glove Mode service
           Doze kernel support
    
    Change-Id: I33fdb04d2131acd284c76993a69fdb342c3a39b0
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- z3c.mk
+++ z3c.mk
@@ -46,8 +46,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/thermanager.xml:system/etc/thermanager.xml
 
 # Doze
+#PRODUCT_PACKAGES += \
+#    SonyDoze
+
+# New Doze pkg
 PRODUCT_PACKAGES += \
-    SonyDoze
+    DeviceSettings
 
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
